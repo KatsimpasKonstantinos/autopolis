0f1d677790e7492aab73a339d45a16687ceececb
Miku <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2025-01-29 02:20:07 +0100

    Update Landing_Page.tsx

--- src/pages/Landing_Page.tsx
+++ src/pages/Landing_Page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Landing_Page() {
     return (
         <div>
             <h1>Welcome to the landing page!</h1>
+            <Link to="/select_game">Select a game</Link>
         </div>
     );
 }
